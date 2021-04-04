f73f41faf20c2e761e8cb215bec80fd0089b1c8b
kabir <kabirsngh0023@gmail.com>
2021-04-05 02:15:37 +0530

    Final deploy prep

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
@@ -13,42 +13,37 @@ const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
 const commentRoutes = require("./routes/comment");
 
-
 const app = express();
 
-mongoose.connect(process.env.DATABASE,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useUnifiedTopology: true
-}).then(()=>(
-    console.log("connected to database")
-));
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to database"));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.get("/",(req,res) => {
-    fs.readFile('docs/apiDocs.json',(err, data) => {
-        if(err){
-            res.status(400).json({
-                error:err
-            })
-        }
-        console.log(data)
-        const docs = JSON.parse(data)
-        res.json(docs);
-    })
-})
-app.use('/api',authRoutes);
-app.use('/api',userRoutes);
-app.use('/p',postRoutes);
-app.use('/api',commentRoutes);
+
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/p", postRoutes);
+app.use("/api", commentRoutes);
 const PORT = process.env.PORT || 8000;
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API Running");
+  });
 }
-app.listen(PORT, ()=>{
-    console.log("Server statrted at port 8000");
-})
+app.listen(PORT, () => {
+  console.log("Server statrted at port 8000");
+});
